09a8d925ca355938839bb6ab09f30ebe9adb715c
Kayky Belleboni Casagrande <kbelleboni@gmail.com>
2023-02-24 20:54:41 -0300

    Add: anyhow lib, test implementation

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,39 @@
-fn main() {}
+use websocket::sync::Server;
+
+fn main() -> anyhow::Result<()> {
+    //    let addr = std::env::args()
+    //        .next()
+    //        .unwrap_or("127.0.0.1:2794".to_owned());
+    //
+    let addr = "127.0.0.1:2794";
+
+    println!("INFO: Runnign webscoket server at {addr}...");
+    let server = Server::bind(addr)?;
+
+    for request in server.filter_map(Result::ok) {
+        println!("Request accepted!");
+    }
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use websocket::{ClientBuilder, OwnedMessage};
+
+    #[test]
+    fn send_message() -> anyhow::Result<()> {
+        let addr = "ws://127.0.0.1:2794";
+        let client = ClientBuilder::new(addr)?.connect_insecure()?;
+
+        let (_, mut sender) = client.split()?;
+
+        let message = OwnedMessage::Text("Oi kk".to_owned());
+
+        dbg!(&message);
+
+        sender.send_message(&message)?;
+
+        Ok(())
+    }
+}
